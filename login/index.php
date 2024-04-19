b6e9948930d8b81b1bb92113129ed3c40455328d
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 18:06:55 -0500

    Update login/index.php to include navbar with line breaks

--- login/index.php
+++ login/index.php
@@ -1,9 +1,10 @@
-<?php // This is a login form index page within the login folder.
+<?php // This is a login form index page within the login folder with navbar with line breaks.
 
 ?>
 <html>
 <head><title>Login Page</title></head>
 <body>
+<nav class="navbar"><a href="../index.php">Home</a><br><a href="index.php">Login</a><br><a href="../about/index.php">About</a></nav>
 <h2>Login Form</h2>
 <form action="login_process.php" method="post">
 <label>Username:</label>

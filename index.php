5dceb88b713187f157f920f429c650f6dd7aeee2
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 18:04:15 -0500

    Update index.php to include link to About page and navbar

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php // This is an index file with a link to the login page.
-
+<?php // This is an index file with a link to the login page and a new navbar.
 ?>
 <html>
 <head><title>Welcome Page</title></head>
 <body>
+<div class="navbar"><a href="index.php">Home</a> | a href="login/index.php">Login</a> | <a href="about/index.php">About</a></div>
 <h1>Hello, World!</h1>
 <p>This is the index page. Click the link below to login.</p>
 <a href="login/index.php">Login</a>
+<p>Also check out our <a href="about/index.php">About Page</a> to learn more about this project.</p>
 </body>
 </html>

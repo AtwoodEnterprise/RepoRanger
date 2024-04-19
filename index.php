ceea08cff7a6ea0d5f3958e98e2fe057ce77cdd7
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 18:34:26 -0500

    Fix navbar URL in index.php

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
 <html>
 <head><title>Welcome Page</title></head>
 <body>
-<div class="navbar"><a href="index.php">Home</a> | a href="login/index.php">Login</a> | <a href="about/index.php">About</a></div>
+<div class="navbar"><a href="index.php">Home</a> | a href="/login/index.php">Login</a> | <a href="/about/index.php">About</a></div>
 <h1>Hello, World!</h1>
 <p>This is the index page. Click the link below to login.</p>
-<a href="login/index.php">Login</a>
-<p>Also check out our <a href="about/index.php">About Page</a> to learn more about this project.</p>
+<p>Also check out our <a href="/about/index.php">About Page</a> to learn more about this project.</p>
 </body>
 </html>

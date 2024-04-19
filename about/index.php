204057725ed1a525be6fa8356c30a4497bc0f24a
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 17:35:40 -0500

    Add about page

---
+++ about/index.php
@@ -0,0 +1,10 @@
+<?php // This is an about page for RepoRanger.
+
+?>
+<html>
+<head><title>About RepoRanger</title></head>
+<body>
+<h2>About RepoRanger </h2>
+<p>RepoRanger is being created to help manage and organize your repositories efficiently. This tool is designed to provide a user-friendly way to navigate and optimize your code processes with minimal hassle.</p>
+</body>
+</html>

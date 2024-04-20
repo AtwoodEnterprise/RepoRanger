bba334da579681340b71aecab2cf93ce130d3486
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 19:09:02 -0500

    Fix login link in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head><title>Welcome Page</title>
 <link rel="stylesheet" type="text/css" href="style.css"></head>
 <body>
-<div class="navbar"><a href="index.php">Home</a> | a href="login/index.php">Login</a> | <a href="about/index.php">About</a></div>
+<div class="navbar"><a href="index.php">Home</a> | <a href="login/index.php">Login</a> | <a href="about/index.php">About</a></div>
 <h1>Hello, World!</h1>
 <p>This is the index page. Click the link below to login.</p>
 <p>Also check out our <a href="about/index.php">About Page</a> to learn more about this project.</p>

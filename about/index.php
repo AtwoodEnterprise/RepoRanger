f5c19fa5fa8bf36e8a5b79dd1a7d3c70b8ae6150
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 19:18:53 -0500

    Update navbar to display horizontally

--- about/index.php
+++ about/index.php
@@ -3,7 +3,7 @@
 <html>
 <head><title>About RepoRanger </title><link rel="stylesheet" type="text/css" href="../style.css"></head>
 <body>
-<nav class="navbar"><a href="../index.php">Home</a><br><a href="../login/index.php">Login</a><br><a href="index.php">About</a></nav>
+<nav class="navbar"><a href="../index.php">Home</a> | <a href="../login/index.php">Login</a> | <a href="index.php">About</a></nav>
 <h2>About RepoRanger </h2>
 <p>RepoRanger is being created to help manage and organize your repositories efficiently. This tool is designed to provide a user-friendly way to navigate and optimize your code processes with minimal hassle.</p>
 </body>

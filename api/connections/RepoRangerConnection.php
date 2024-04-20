fe94d15451bb0e83b1e334a7f08525945486457c
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 19:38:27 -0500

    Update RepoRangerConnection.php to use variables from config.php

--- api/connections/RepoRangerConnection.php
+++ api/connections/RepoRangerConnection.php
@@ -1,9 +1,10 @@
 <?php
+require once ('../config.php'); // Include config file which is gitignored
 class RepoRangerConnection {
     private $conn;
 
     function __construct() {
-        $this->conn = new mysqli('localhost', 'username', 'password', 'database_name');
+        $this->conn = new mysqli($host, $username, $password, $db_name);
 
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);

9ea52613bf42d0ac30427ce349567ebb2bd90de2
PatrickF97 <99094273+PatrickF97@users.noreply.github.com>
2024-04-19 19:05:25 -0500

    Add CSS link to index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php // This is an index file with a link to the login page and a new navbar.
 ?>
 <html>
-<head><title>Welcome Page</title></head>
+<head><title>Welcome Page</title>
+<link rel="stylesheet" type="text/css" href="style.css"></head>
 <body>
-<div class="navbar"><a href="index.php">Home</a> | a href="/login/index.php">Login</a> | <a href="/about/index.php">About</a></div>
+<div class="navbar"><a href="index.php">Home</a> | a href="login/index.php">Login</a> | <a href="about/index.php">About</a></div>
 <h1>Hello, World!</h1>
 <p>This is the index page. Click the link below to login.</p>
-<p>Also check out our <a href="/about/index.php">About Page</a> to learn more about this project.</p>
+<p>Also check out our <a href="about/index.php">About Page</a> to learn more about this project.</p>
 </body>
 </html>
